8aba0dde93e8593c137989d94c2bb4fe49719b8d
1101137213 <scientologistwlh@gmail.com>
2015-04-17 17:16:52 +0800

    ADO wtih LIST

---
+++ KuasCore/Interceptors/UpdateAllEmployeeNameInterceptor.cs
@@ -0,0 +1,33 @@
+﻿using AopAlliance.Intercept;
+using KuasCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuasCore.Interceptors
+{
+    public class UpdateAllEmployeeNameInterceptor : IMethodInterceptor
+    {
+        public object Invoke(IMethodInvocation invocation)
+        {
+            Console.WriteLine("UpdateAllEmployeeNameInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+            Debug.Print("UpdateAllEmployeeNameInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+
+            /*extra trial*/
+            IList<Employee> result = (IList<Employee>)invocation.Proceed();
+            for (int i = 0; i < result.Count; i++)
+            {
+                Debug.Print(result[i].Name);
+                result[i].Name += " HAHAHA" + i;
+            }
+
+            Console.WriteLine("回傳的資料已取得 [{0}]", result);
+            Debug.Print("回傳的資料已取得 [{0}]", result);
+
+            return result;
+        }
+    }
+}
